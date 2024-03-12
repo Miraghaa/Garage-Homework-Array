9942c5dfc0c15e4ce76796275dfd85a871408744
Miraga99 <miraga041@gmail.com>
2024-03-12 18:39:37 +0400

    Add project files.

---
+++ ArrayMethod/Program.cs
@@ -0,0 +1,78 @@
+﻿
+// 1 ci tapsiriq 
+
+//int[] ints = { 5, 4, 8, 9, 7, 4, 6, 7 };
+
+//void CustomSort(int[] array)
+//{
+
+//    for (int i = 0; i < array.Length; i++)
+//    {
+//        for (int j = i+1; j < array.Length; j++)
+//        {
+//            if (array[i] > array[j])
+//            {
+//                int c = array[i];
+//                array[i] = array[j];
+//                array[j] = c;
+//            }
+//        }
+//    }
+//    foreach (int item  in array)
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+
+//Arrayy(ints);
+
+
+
+// 2ci tapsiriq
+
+//string[] words = { "Miragha", "Pasha", "Fatali" };
+
+//void CustomSearchLetter(string[] words, char letter)
+//{
+//    for (int i = 0; i < words.Length; i++)
+//    {
+//        for (int j = 0; j < words[i].Length; j++)
+//        {
+//            if (words[i].ToLower()[j] == letter)
+//            {
+//                Console.WriteLine(words[i]);
+//            }
+//        }
+//    }
+//}
+
+//CustomSearchLetter(words, 'p');
+
+
+
+// 3 cu tapsiriq    
+
+//int[] Reverse = { 5, 4, 6, 7, 8 };
+
+
+//void CustomReverseMethod(int[] array)
+//{
+//    int[] CustomReverse = { };
+
+//    for (int i = Reverse.Length - 1; i >= 0; i--)
+//    {
+//        Array.Resize(ref CustomReverse, CustomReverse.Length + 1);
+//        CustomReverse[CustomReverse.Length - 1] = Reverse[i];
+//    }
+//    foreach (int i in CustomReverse)
+//    {
+//        Console.WriteLine(i);
+//    }
+//};
+
+//CustomReverseMethod(Reverse);
+
+
+
+
+
